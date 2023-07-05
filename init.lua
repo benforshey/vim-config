ae3226e6a9322a2f9a22f43ada7c1fd6e9925802
Ben Forshey <benjamin.a.forshey@nasa.gov>
2023-07-05 11:35:54 -0400

    feat: add modules to init, nohl

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 require "options"
+require "plugins"
 require "autocommands"
+require "keymaps"
 
 vim.cmd("source ~/.config/nvim/viml/vscode.vim")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,5 @@ local keymap = vim.keymap.set
 
 -- Set up space for leader
 keymap({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+keymap({ "n", "v" }, "<leader>h", "<cmd>nohl<cr>", { silent = true })
