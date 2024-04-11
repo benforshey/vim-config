52fe6856cf622d8c771649198a28fe14aba5feea
Ben Forshey <benjamin.a.forshey@nasa.gov>
2024-04-11 16:43:44 -0400

    feat: restore `gx`; remove deprecated null-ls

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -19,4 +19,44 @@ return {
       },
     },
   },
+  {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    dependencies = "nvim-lua/plenary.nvim",
+    submodules = false, -- not needed, submodules are required only for tests
+    opts = {
+      open_browser_app = "open", -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
+      open_browser_args = { "--background" }, -- specify any arguments, such as --background for macOS' "open".
+      handlers = {
+        plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
+        github = true, -- open github issues
+        brewfile = true, -- open Homebrew formulaes and casks
+        package_json = true, -- open dependencies from package.json
+        search = true, -- search the web/selection on the web if nothing else is found
+        jira = { -- custom handler to open Jira tickets (these have higher precedence than builtin handlers)
+          handle = function(mode, line, _)
+            local ticket = require("gx.helper").find(line, mode, "(%u+-%d+)")
+            if ticket and #ticket < 20 then
+              return "http://jira.company.com/browse/" .. ticket
+            end
+          end,
+        },
+        rust = { -- custom handler to open rust's cargo packages
+          filetype = { "toml" }, -- you can also set the required filetype for this handler
+          filename = "Cargo.toml", -- or the necessary filename
+          handle = function(mode, line, _)
+            local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
+
+            if crate then
+              return "https://crates.io/crates/" .. crate
+            end
+          end,
+        },
+      },
+      handler_options = {
+        search_engine = "google", -- google, bing, duckduckgo, or ecosia or you can pass in a custom search engine, e.g., "https://search.brave.com/search?q="
+      },
+    },
+  },
 }
