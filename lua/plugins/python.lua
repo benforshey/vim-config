d599653d742aaf0866e9d8fb6c3e2461c78908aa
Ben Forshey <benjamin.a.forshey@nasa.gov>
2024-06-14 09:37:23 -0400

    feat(config): update with nvim upgrade, lazvim upgrade

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "debugpy",
-        "pyright",
-        "ruff",
-        "ruff-lsp",
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        python = { "ruff_fix", "ruff_format" },
-      },
-    },
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "debugpy",
+  --       "pyright",
+  --       "ruff",
+  --       "ruff-lsp",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       python = { "ruff_fix", "ruff_format" },
+  --     },
+  --   },
+  -- },
 }
